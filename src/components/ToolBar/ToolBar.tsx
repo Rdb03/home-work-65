2281db28053e4f32cec20d931b22dded1d8c557a
Djaparov Ramil <rdb03@mail.ru>
2023-12-06 15:56:33 +0600

    Отредактировал вкладку about

--- src/components/ToolBar/ToolBar.tsx
+++ src/components/ToolBar/ToolBar.tsx
@@ -10,7 +10,7 @@ const ToolBar = () => {
             </div>
             <div className='nav-link-div'>
                 <NavLink className='nav-link' to='/pages/home'>Home</NavLink>
-                <NavLink className='nav-link'to='/pages/about'>About</NavLink>
+                <NavLink className='nav-link' to='/pages/about'>About</NavLink>
                 <NavLink className='nav-link' to='/pages/contacts'>Contacts</NavLink>
                 <NavLink className='nav-link' to='/pages/division'>Divisions</NavLink>
                 <NavLink className='nav-link' to='/pages/admin'>Admin</NavLink>
